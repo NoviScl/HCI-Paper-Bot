d3fc1aabb15d905ac0abaa97d478cbf1b9b2e8eb
NoviScl <sichenglei1125@gmail.com>
2024-04-23 11:38:25 -0700

    update dependency

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is simple paper bot that helps you filter through the past 6.5 years of CHI
 
 ### Usage
 
-1. Clone the repository.
+1. Clone the repository. Install dependencies: `pip install openai anthropic`
 
 2. Download the SIGCHI data from [this link](https://drive.google.com/file/d/1Yns5_cL0h6lnaVpRCsz8go7vYqXStU6R/view?usp=sharing) and unzip it. 
 
@@ -12,4 +12,4 @@ This is simple paper bot that helps you filter through the past 6.5 years of CHI
 
 4. Put your OpenAI / Anthropic API keys in `keys.json`. For OpenAI users, put your API key in `api_key` (you can also additionally specify `organization_id`). For Anthropic users, put your API key in `anthropic_key`. 
 
-5. Run the bot using `python paper_filter_bot.py`.
+5. Run the bot using `python3 paper_filter_bot.py`.
--- paper_filter_bot.py
+++ paper_filter_bot.py
@@ -10,16 +10,16 @@ import retry
 @retry.retry(tries=3, delay=2)
 def paper_filter(paper, client, model, seed):
     ## use LLM to decide whether the paper satisfied the criteria 
-    prompt = "You are a helpful literature review assistant whose job is to read the below paper and help me decide if it satisfies my criteria. The criteria are:\n"
-    prompt += "(1) The paper conducts both quantitative surveys and qualitative interviews. The paper's main contribution should be drawing insights from the survey and interview results.\n"
+    prompt = "You are a helpful literature review assistant whose job is to read the below paper and help me decide if it satisfies all my criteria. The criteria are:\n"
+    prompt += "(1) The paper is about conducting both large-scale quantitative surveys and in-depth qualitative interviews with human participants. The paper's main contribution should be to draw insights from the survey and interview results (conducting human evaluation of a model or system does not count).\n"
     prompt += "(2) The participants involved are all recruited from online crowdsourcing platforms, such as Amazon Mechanical Turk or Prolific. And the participants shouldn't be from special backgrounds such as domain experts in niche areas.\n"
-    prompt += "(3) The surveys or interviews do not require participants to use any special equipment or software, such as VR headsets or internal chat apps.\n"
+    prompt += "(3) The surveys and interviews do not require participants to use any special equipment or software, such as VR headsets or internal apps.\n"
     prompt += "(4) The surveys and interviews should not be overly long, for example, involving multi-day longitudinal tracking. Participants should be able to finish the survey or interview within an hour.\n"
     prompt += "(5) The research questions that the paper is trying to answer should not be very time-sensitive, such as studying the impact of COVID-19.\n\n"
     prompt += "The paper is:\n" 
     prompt += "Title: " + paper["title"].strip() + "\n" 
     prompt += "Abstract: " + paper["abstract"].strip() + "\n\n"
-    prompt += "Please give me a binary answer (yes/no) on whether this paper satisfies both criteria. Do not include any other explanation, the output should be either yes or no."
+    prompt += "Please give me a binary answer (yes/no) on whether this paper satisfies all the criteria (you should say no as long as any one of the criteria is not met). Do not include any other explanation, the output should be either yes or no."
     
     prompt_messages = [{"role": "user", "content": prompt}]
     response, cost = call_api(client, model, prompt_messages, temperature=0., max_tokens=1, seed=seed, json_output=False)
@@ -52,6 +52,14 @@ if __name__ == "__main__":
     #     {
     #         "title": "Will AI Console Me when I Lose my Pet? Understanding Perceptions of AI-Mediated Email Writing",
     #         "abstract": "Large language models are increasingly mediating, modifying, and even generating messages for users, but the receivers of these messages may not be aware of the involvement of AI. To examine this emerging direction of AI-Mediated Communication (AI-MC), we investigate people's perceptions of AI written messages. We analyze how such perceptions change in accordance with the interpersonal emphasis of a given message. We conducted both large-scale surveys and in-depth interviews to investigate how a diverse set of factors influence people's perceived trust in AI-mediated writing of emails. We found that people's trust in email writers decreased when they were told that AI was involved in the writing process. Surprisingly trust increased when AI was used for writing more interpersonal emails (as opposed to more transactional ones). Our study provides insights regarding how people perceive AI-MC and has practical design implications on building AI-based products to aid human interlocutors in communication."
+    #     },
+    #     {
+    #         "title": "I Like Their Autonomy and Closeness to Me: Uncovering the Perceived Appeal of Social-Media Influencers",
+    #         "abstract": "The proliferation of influencers on social-media platforms has drawn considerable research attention, particularly in the field of marketing. Nevertheless, there is limited understanding among HCI and communication researchers of what leads these social-media influencers’ (SMIs’) audiences to favor and choose their content over traditional media. To fill this gap, we conducted semi-structured interviews with 45 SMI audience members. Our findings revealed a total of eight categories of SMIs’ appeals, i.e., factors that made the interviewees favor their content over traditional media. These appeals can further be grouped into three categories: content, presentation, and closeness. In particular, we identified the key role of SMIs’ perceived high autonomy and independence, which led both their content and their presentation styles to be seen as distinct from and more appealing than traditional media. Likewise, four closeness appeals made our participants feel emotionally attached to SMIs, resulting in sustained engagement."
+    #     },
+    #     {
+    #         "title": "Interface Design for Crowdsourcing Hierarchical Multi-Label Text Annotations",
+    #         "abstract": "Human data labeling is an important and expensive task at the heart of supervised learning systems. Hierarchies help humans understand and organize concepts. We ask whether and how concept hierarchies can inform the design of annotation interfaces to improve labeling quality and efficiency. We study this question through annotation of vaccine misinformation, where the labeling task is difficult and highly subjective. We investigate 6 user interface designs for crowdsourcing hierarchical labels by collecting over 18,000 individual annotations. Under a fixed budget, integrating hierarchies into the design improves crowdsource workers' F1 scores. We attribute this to (1) Grouping similar concepts, improving F1 scores by +0.16 over random groupings, (2) Strong relative performance on high-difficulty examples (relative F1 score difference of +0.40), and (3) Filtering out obvious negatives, increasing precision by +0.07. Ultimately, labeling schemes integrating the hierarchy outperform those that do not - achieving mean F1 of 0.70."
     #     }
     # ]
 
